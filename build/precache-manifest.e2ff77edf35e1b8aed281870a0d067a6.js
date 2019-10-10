b7e0034faebcc94f42414fa431ed1a5cf90f0419
Justin <justinzdeng@gmail.com>
2019-10-10 15:07:11 -0400

    My first commit

---
+++ build/precache-manifest.e2ff77edf35e1b8aed281870a0d067a6.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "5832aa6150fab93a07f6",
+    "url": "/static/css/main.8805a1d3.chunk.css"
+  },
+  {
+    "revision": "5832aa6150fab93a07f6",
+    "url": "/static/js/main.f8e85b18.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "5b751f9b56882e029c8f",
+    "url": "/static/js/2.bcab3319.chunk.js"
+  },
+  {
+    "revision": "2ec1879e7dcb111229fb5bd968d1872d",
+    "url": "/index.html"
+  }
+];
